0abd155aafefa8de36fc5aeef7fdd1d7da7950c9
Dave Cross <dave@dave.org.uk>
2014-01-28 13:46:04 +0000

    Added first test

---
+++ t/Test/Lystyng/Schema.pm
@@ -0,0 +1,22 @@
+package Test::Lystyng::Schema;
+
+use base 'Test::Class';
+use Test::More;
+
+use lib 'lib';
+
+sub check_connect : Test(2) {
+  BAIL_OUT('Missing connection info')
+    unless $ENV{LYSTYNG_DB_SERVER} && $ENV{LYSTYNG_DB_NAME} &&
+           $ENV{LYSTYNG_DB_USER}   && $ENV{LYSTYNG_DB_PASS};
+
+  use_ok('Lystyng::Schema');
+
+  my $sch = Lystyng::Schema->connect(
+    "dbi:mysql:hostname=$ENV{LYSTYNG_DB_SERVER};database=$ENV{LYSTYNG_DB_NAME}",
+    $ENV{LYSTYNG_DB_USER}, $ENV{LYSTYNG_DB_PASS}
+  );
+
+  ok($sch);
+}
+
