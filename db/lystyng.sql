4c88009149984549462ef855340b6860de99ad6b
Dave Cross <dave@dave.org.uk>
2014-02-03 13:06:43 +0000

    Added 'drop' statements to DDL

--- db/lystyng.sql
+++ db/lystyng.sql
@@ -1,3 +1,4 @@
+drop table if exists `user`;
 create table `user` (
   id integer primary key auto_increment,
   username varchar(20) not null,
@@ -6,6 +7,7 @@ create table `user` (
   password char(64) not null
 ) ENGINE=INNODB CHARSET=utf8;
 
+drop table if exists `list`;
 create table list (
   id integer primary key auto_increment,
   title varchar(200) not null,
@@ -15,6 +17,7 @@ create table list (
   foreign key (user) references `user`(id)
 ) ENGINE=INNODB CHARSET=utf8;
 
+drop table if exists `list_item`;
 create table list_item (
   id integer primary key auto_increment,
   title varchar(200) not null,
