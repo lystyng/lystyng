23fb24a27a52d3086214d3086622151dfb2659f2
Dave Cross <dave@dave.org.uk>
2016-10-13 13:37:52 +0100

    Added useful methods to the model class

--- lib/Lystyng/Model.pm
+++ lib/Lystyng/Model.pm
@@ -23,4 +23,43 @@ sub _build_user_rs {
   return $_[0]->schema->resultset('User');
 }
 
+sub get_all_users {
+  my $self = shift;
+  return $self->user_rs->all;
+}
+
+sub get_user_by_username {
+  my $self = shift;
+  my $username = (@_);
+
+  return $self->user_rs->find({
+    username => $username,
+  }, {
+    prefetch => 'lists',
+  });
+}
+
+sub get_user_list_by_slug {
+  my $self = shift;
+  my ($user, $slug) = @_;
+  
+  return $user->lists->find({
+    slug => $slug,
+  });
+}
+
+sub add_user_list {
+  my $self = shift;
+  my ($user, $list_data) = @_;
+  
+  $user->add_to_lists($list_data);
+}
+
+sub add_user {
+  my $self = shift;
+  my ($user_data) = @_;
+  
+  $self->user_rs->create($user_data);
+}
+
 1;
